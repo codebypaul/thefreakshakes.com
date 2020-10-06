5906b61a9b2bd43a0763a1481164f37e5d8b0b26
Paul Williams <65035460+codebypaul@users.noreply.github.com>
2020-10-06 18:19:23 -0400

    Update CoverCard.js

--- src/components/CoverCard.js
+++ src/components/CoverCard.js
@@ -5,8 +5,10 @@ import FreakShake from '../Unicorn_Dazzle.JPG'
 export default class Covercard extends Component {
     render() {
         return (
-            <div className="container-fluid home-img-container d-flex justify-content-center p-5">
-                <img className="freakShake" src={FreakShake} alt="freakshake"/>
+            <div className="container-fluid d-flex justify-content-center p-5">
+                <div class="img-container">
+                    <img className="freakShake" src={FreakShake} alt="freakshake"/>
+                </div>
             </div>
         )
     }
