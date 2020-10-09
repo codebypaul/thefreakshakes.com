04421efb3146fa89b15f64ea1b58b167cb6dbfe6
Paul Williams <65035460+codebypaul@users.noreply.github.com>
2020-10-08 23:10:05 -0400

    1 more update for mobile

--- src/components/CoverCard.js
+++ src/components/CoverCard.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react'
 import FreakShake from '../Unicorn_Dazzle.JPG'
-
+import styled from 'styled-components'
 
 export default class Covercard extends Component {
     render() {
         return (
-            <div className="col-xl mx-auto p-5">
-                <div className="card">
+            <Cover className="p-2 bg-dark">
+                <div className="mx-auto col-sm-4 col-lg-4 bg-secondary p-3">
                     <div class="img-container">
-                        <img className="freakShake" src={FreakShake} alt="freakshake" className="card-img-top"/>
+                        <img src={FreakShake} alt="freakshake" className="card-img freakShake"/>
                     </div>
                 </div>
                 
-            </div>
+            </Cover>
         )
     }
 }
 
+const Cover = styled.div`
+
+.img-container{
+    position: relative;
+    overflow: hidden;
+    
+}
+`
